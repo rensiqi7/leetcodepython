37835900471158a6e147382439797ea1f771199a
shiki <rensiqi384906149@yahoo.co.jp>
2018-06-17 23:06:23 +0900

    solution

--- 34search_for_a_range.py
+++ 34search_for_a_range.py
@@ -1,37 +1,31 @@
-class Solution(object):
+class Solution:
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        result = []
-        length = len(nums)
-
-        start = 0
-        end = length
-        while start < end:
-            mid = (start + end) // 2
-            if nums[mid] == target and (mid == 0 or nums[mid - 1] != target):
-                result.append(mid)
-                break
-            if nums[mid] < target:
-                start = mid + 1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                end = mid
-        if not result:
-            return [-1, -1]
-
-        end = length
-        while start < end:
-            mid = (start + end) // 2
-            if nums[mid] == target and (mid == length - 1 or nums[mid + 1]
-                                        != target):
-                result.append(mid)
-                break
-            if nums[mid] <= target:
-                start = mid + 1
-            else:
-                end = mid
-
-        return result
+                list = [0, 0]
+                if nums[left] == target:
+                    list[0] = left
+                if nums[right] == target:
+                    list[1] = right
+                for i in range(mid, right + 1):
+                    if nums[i] != target:
+                        list[1] = i - 1
+                        break
+                for i in range(mid, left - 1, -1):
+                    if nums[i] != target:
+                        list[0] = i + 1
+                        break
+                return list
+        return [-1, -1]
