3eb956ab3ace80c02b2496a2860a4de3118812b4
shiki <rensiqi384906149@yahoo.co.jp>
2018-06-28 21:04:56 +0900

    solution

---
+++ 56merge_intervals.py
@@ -0,0 +1,29 @@
+# Definition for an interval.
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution:
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        inters = sorted(intervals, key=lambda intval: intval.start)
+        ret = []
+        n = len(intervals)
+        if n == 0:
+            return ret
+        s = inters[0].start
+        e = inters[0].end
+        for i in range(1, n):
+            if inters[i].start <= e:
+                e = max(inters[i].end, e)
+            else:
+                ret.append(Interval(s, e))
+                s = inters[i].start
+                e = inters[i].end
+        ret.append(Interval(s, e))
+        return ret
