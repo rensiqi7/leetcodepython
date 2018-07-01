78889489a5311bd9dbf4b346b135e58ae58f2c31
shiki <rensiqi384906149@yahoo.co.jp>
2018-07-01 12:32:37 +0900

    solution

---
+++ 74search_a_2d_matrix.py
@@ -0,0 +1,54 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        row = len(matrix)
+        if row == 0:
+            return False
+        column = len(matrix[0])
+        if column == 0:
+            return False
+        i = 0
+        while i < row:
+            if target <= matrix[i][column - 1]:
+                for j in range(column - 1, -1, -1):
+                    if matrix[i][j] == target:
+                        return True
+                    else:
+                        pass
+                return False
+            else:
+                i += 1
+        return False
+
+
+'''
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        if m == 0:
+            return False
+
+        n = len(matrix[0])
+        if n == 0:
+            return False
+
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+
+        return False
+'''
