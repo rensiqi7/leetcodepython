15265fdf4b066f60677fc017b2cb98ec4d37c8c2
shiki <rensiqi384906149@yahoo.co.jp>
2018-08-07 14:07:31 +0900

    solution

---
+++ 78subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        for num in sorted(nums):
+            result += [item + [num] for item in result]
+        return result
