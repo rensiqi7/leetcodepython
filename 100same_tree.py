d7bcbdbde1d483bbf0818ee45c0ca8d5d7bc20af
shiki <rensiqi384906149@yahoo.co.jp>
2018-08-10 11:00:46 +0900

    solution

--- 100same_tree.py
+++ 100same_tree.py
@@ -20,4 +20,26 @@ class Solution:
         elif p.val != q.val:
             return False
         else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(q.right, q.right)
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+'''
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        # insight: use recursive solution
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+
+        left_is_same = self.isSameTree(p.left, q.left)
+        right_is_same = self.isSameTree(p.right, q.right)
+        root_is_same = p.val == q.val
+
+        return root_is_same and left_is_same and right_is_same
+'''
