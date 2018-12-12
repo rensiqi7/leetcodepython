de1aa52a60a0f3ff645ba43a938ee20d9c3bdc4d
rensiqi7 <rensiqi384906149@yahoo.co.jp>
2018-12-12 16:10:20 +0900

    solution

---
+++ 6zig_zag_conversion.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+        row, direction, res = 0, -1, [""] * numRows
+        for char in s:
+            res[row] += char
+            if row == 0 or row == numRows - 1:
+                direction *= -1
+            row += direction
+        return "".join(res)
